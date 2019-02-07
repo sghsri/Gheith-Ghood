645a813ebfa661c1ffc33f9700a4dbe83de33b6a
sghsri <sghsri@gmail.com>
2019-02-07 00:22:18 -0600

    highlight column on hover

--- js/content.js
+++ js/content.js
@@ -1,12 +1,32 @@
 if(window.location.href.includes('cs439')){
+    // $('body').prepend('<button id="hide">Hello</button>');
+
     $('table').find('tr').each(function(){
         $(this).find('td').css('font-size','15px');
         $(this).find('td').each(function(){
             var color = $(this).css('background-color')
+            console.log(color);
             if(color == 'rgb(255, 0, 0)'){
-                $(this).css('background-color','#FF5252');
+                $(this).addClass('fail');
+            } else if(color == 'rgb(0, 128, 0)'){
+                $(this).addClass('success');
             }
+            $(this).css('background-color','');
         });
     });
+    $('table').on('mouseover','td',function(){
+        var index = $(this).index();
+        $('table').find('tr').each(function(){
+            $(this).find(`td:eq(${index})`).each(function(){
+                $(this).addClass('hovered');
+            });
+        });
+    })
+    $('table').on('mouseout','td',function(){
+        $('td').removeClass('hovered');
+    })
+    // $('#hide').click(function(){
+    //     $('')
+    // });
 
 }
