fe9be2e673d6c00614f02c6954a669ba5808920e
sghsri <sghsri@gmail.com>
2019-02-06 20:00:58 -0600

    drop shadow, color changes

--- js/content.js
+++ js/content.js
@@ -1,1 +1,12 @@
-console.log('hello');
+if(window.location.href.includes('cs439')){
+    $('table').find('tr').each(function(){
+        $(this).find('td').css('font-size','15px');
+        $(this).find('td').each(function(){
+            var color = $(this).css('background-color')
+            if(color == 'rgb(255, 0, 0)'){
+                $(this).css('background-color','#FF5252');
+            }
+        });
+    });
+
+}
